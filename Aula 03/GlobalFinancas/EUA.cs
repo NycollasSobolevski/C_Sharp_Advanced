3cbbca8ac192a034d44e58b6f6fe5068979b4a19
Nycollas <sobolevski.academy@gmail.com>
2023-05-18 11:46:31 -0300

    ADD: aula 04 conteudo

---
+++ Aula 03/GlobalFinancas/EUA.cs
@@ -0,0 +1,9 @@
+public class EuaDimissalProcess : DismissalProcess
+{
+    public override string Title => "employee dismissal";
+
+    public override void Apply(DismissalArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Aula 04/main.cs
@@ -0,0 +1,11 @@
+using System;
+using static FunctionUtil;
+
+class Program {
+  public static void Main (string[] args)
+  {
+    var f = sin(x) * sin(x) + cos(x) * cos(x) + e;
+
+    Console.WriteLine(f);
+  }
+}
