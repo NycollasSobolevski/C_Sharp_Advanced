09a6f42126e1877438138a70f2a72ea09711b330
Nycollas <sobolevski.academy@gmail.com>
2023-06-01 11:31:40 -0300

    ADD: Aula 09/ entity

---
+++ Aula 09/BancoExemplo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+string dataSource = @"CT-C-0018C\SQLEXPRESS";   
+
+// SqlConnectionStringBuilder stringConnectionBuilder = new SqlConnectionStringBuilder();
+// stringConnectionBuilder.DataSource = dataSource; // Nome do servidor
+// stringConnectionBuilder.InitialCatalog = "example"; // Nome do banco
+// stringConnectionBuilder.IntegratedSecurity = true;
+// string stringConnection = stringConnectionBuilder.ConnectionString;
+
+// SqlConnection conn = new SqlConnection(stringConnection);
+// conn.Open();
+
+// SqlCommand comm = new SqlCommand("insert Cliente values ('Pamella', '123', CONVERT(DATETIME, '30/12/2023'));");
+// comm.Connection = conn;
+// comm.ExecuteNonQuery();
+// conn.Close();
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+SqlConnectionStringBuilder stringConnectionBuilder = new SqlConnectionStringBuilder();
+stringConnectionBuilder.DataSource = dataSource;
+stringConnectionBuilder.InitialCatalog = "example";
+stringConnectionBuilder.IntegratedSecurity = true;
+string stringConnection = stringConnectionBuilder.ConnectionString;
+SqlConnection conn = new SqlConnection(stringConnection);
+conn.Open();
+
+string nome = Console.ReadLine();
+string senha = Console.ReadLine();
+
+nome = "'; drop table Cliente; --";
+
+SqlCommand comm = new SqlCommand($"select * from Cliente where Nome = '{nome}' and Senha = '{senha}'");
+comm.Connection = conn;
+var reader = comm.ExecuteReader();
+DataTable dt = new DataTable();
+dt.Load(reader);
+if (dt.Rows.Count > 0)
+    Console.WriteLine($"Usuário {dt.Rows[0].ItemArray[0]} Logado");
+else
+    Console.WriteLine("Conta inexistente");
+conn.Close();
